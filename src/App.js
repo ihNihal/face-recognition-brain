e260f0696077d879193af3180ab60224f596b52c
Ihnihal <istiaque98@gmail.com>
2023-07-29 11:40:40 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+// import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import Clarifai from "clarifai";
+// import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+
+// const app = new Clarifai.App({
+//   apiKey: "549839ae28b1472b9061bcac2070cd6c",
+// });
+
+function App() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  return (
+    <div className="App">
+      {/* <Particles className="particles" params={particleOptions} /> */}
+
+      <Particles
+        className="particles"
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#linear-gradient(89deg, #ff5edf 0%, #04c8de 100%);",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div className="white f3">
+      <div>{"Nihal, Your Current Rank Is..."}</div>
+      <div>{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
